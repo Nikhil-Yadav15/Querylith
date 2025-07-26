ab2c5c5e89db3ddc4ed7751ee26a22a61273fc96
Nikhil Yadav <70308863+Nikhil-Yadav15@users.noreply.github.com>
2025-07-26 14:02:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 from fastapi import FastAPI, Header, HTTPException
 from models import QuestionRequest, AnswerResponse
-from rag import ingest, answer
+from rag import ingest_and_answer_then_cleanup
+import uvicorn
+from dotenv import load_dotenv
+load_dotenv()
+
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 API_KEY_EXPECTED = os.getenv("HACKRX_API_KEY")
 
@@ -14,11 +24,8 @@ def run_qa(payload:QuestionRequest, authorization: str = Header(...)):
     if authorization.removeprefix("Bearer ").strip() != API_KEY_EXPECTED:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-    vectordb = ingest(payload.documents)
-    answers  = answer(vectordb, payload.questions)
 
+    logger.info("Ingesting and Answering...")
+    answers = ingest_and_answer_then_cleanup(payload.documents, payload.questions)
+    logger.info("Answered")
     return {"answers": answers}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
